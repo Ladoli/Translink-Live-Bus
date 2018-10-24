a5e3494da79857cb18a7cde5e70881f3ee0b9c94
Villadolid <villadolida@student.douglascollege.ca>
2018-10-24 14:18:14 -0700

    Modified Bus Marker color and size, Added Map Options Bar, Bus Route Toggle Button

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -4,6 +4,7 @@ import { isEmpty, map } from 'lodash';
 import { inject, observer } from 'mobx-react';
 import * as React from 'react';
 import ReactMapGL,{ Marker } from 'react-map-gl';
+import { Button } from 'semantic-ui-react';
 import { BUS_REFRESH_INTERVAL, MAPBOX_TOKEN, NODE_API_LINK } from '../../config/config.js';
 import { busStore } from '../../store/busStore';
 import './map.css';
@@ -19,7 +20,8 @@ const initialState = {
         width: 400,
         zoom: 11,
     },
-    busses: [{Latitude: 0, Longitude: 0}]
+    busses: [{Latitude: 0, Longitude: 0}],
+    busRouteDisplayed: true
 };
 
 interface InjectedProps {
@@ -88,7 +90,16 @@ class Map extends React.Component<{}, State> {
         }));
     };
 
+    public toggleBusRoute = () =>{
+      if(this.state.busRouteDisplayed){
+        this.setState({busRouteDisplayed:false});
+      }else{
+        this.setState({busRouteDisplayed:true});
+      }
+    }
+
     public render() {
+        this.toggleBusRoute = this.toggleBusRoute.bind(this);
         const busList = this.injected.busStore.getBusList;
         const { viewport } = this.state;
         if(isEmpty(busList)){
@@ -103,6 +114,7 @@ class Map extends React.Component<{}, State> {
         }
 
         return (
+          <div>
             <ReactMapGL
                 {...viewport}
                 mapboxApiAccessToken={MAPBOX_TOKEN}
@@ -112,11 +124,23 @@ class Map extends React.Component<{}, State> {
             {
               map(busList,(value,key)=>{
                     return (<Marker key={key} latitude={value.Latitude} longitude={value.Longitude}>
-                      <div className="marker"/>
+                      <div className="marker">
+                        { this.state.busRouteDisplayed && (
+                          <div className="markerBusInfo">{value.RouteNo}</div>
+                          )
+                        }
+                      </div>
                     </Marker>);
                 })
             }
             </ReactMapGL>
+            <div className="mapOptionsControlBar">
+              <div className="flexCenterAll" style={{width:"100%", height: "100%"}}>
+                <Button onClick={this.toggleBusRoute} compact={true} color="blue" className="mapMenuButton">Toggle Bus Route Display</Button>
+              </div>
+            </div>
+          </div>
+
         );
     }
 }
